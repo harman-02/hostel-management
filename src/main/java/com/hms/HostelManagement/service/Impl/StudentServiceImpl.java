37d03b6cd9058ee5271a68688ac822b6b2c5bccf
Saikat <saikat.mondal.cse21@itbhu.ac.in>
2023-10-18 01:57:10 +0530

    service layer implementation

---
+++ src/main/java/com/hms/HostelManagement/service/Impl/StudentServiceImpl.java
@@ -0,0 +1,34 @@
+package com.hms.HostelManagement.service.Impl;
+
+
+import com.hms.HostelManagement.model.Student;
+import com.hms.HostelManagement.repository.StudentRepository;
+import com.hms.HostelManagement.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+    @Override
+    public Student getStudentByRollNo(String rollNo) {
+        Student student = studentRepository.getByRollNo(rollNo);
+        return student;
+    }
+
+    @Override
+    public void deleteStudent(Student student) {
+        studentRepository.delete(student.getRollNo());
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+        studentRepository.update(student);
+    }
+}
--- src/main/java/com/hms/HostelManagement/service/StudentService.java
+++ src/main/java/com/hms/HostelManagement/service/StudentService.java
@@ -1,27 +1,11 @@
 package com.hms.HostelManagement.service;
 
 import com.hms.HostelManagement.model.Student;
-import com.hms.HostelManagement.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class StudentService {
-    @Autowired
-    private StudentRepository studentRepository;
+public interface StudentService {
+    public Student getStudentByRollNo(String rollNo);
 
+    public void deleteStudent(Student student);
 
-
-    public Student getStudentByRollNo(String rollNo) {
-        Student student = studentRepository.getByRollNo(rollNo);
-        return student;
-    }
-
-    public void deleteStudent(Student student) {
-        studentRepository.delete(student.getRollNo());
-    }
-
-    public void updateStudent(Student student) {
-        studentRepository.update(student);
-    }
+    public void updateStudent(Student student) ;
 }
