2048366d446fb13950e0ee120805a65a9a01eab2
836hardik-agrawal <hardik.agrawal.cse21@itbhu.ac.in>
2023-10-22 17:13:04 +0530

    mess controller

--- src/main/java/com/hms/HostelManagement/controller/MessCancellationsController.java
+++ src/main/java/com/hms/HostelManagement/controller/MessCancellationsController.java
@@ -1,5 +1,7 @@
 package com.hms.HostelManagement.controller;
 
+import com.hms.HostelManagement.model.Hostel;
+import com.hms.HostelManagement.model.MessCancellations;
 import com.hms.HostelManagement.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
@@ -44,5 +48,33 @@ public class MessCancellationsController extends BaseController{
         addDefaultAttributes(model, httpSession);
         model.addAttribute("cancellation", messCancellationsService.getAll());
         return "messCancellations/allMessCancellations";
+    }
+    @GetMapping("/mess/add")
+    public String addMess(Model model, HttpSession session){
+        if (!isAuthenticated(session)) {
+            return "redirect:/";
+        }
+        addDefaultAttributes(model, session);
+
+        MessCancellations mess = new MessCancellations();
+
+        model.addAttribute("messAdd", mess);
+          String s;
+        return    "messCancellations/addMessCancellations";
+
+    }
+    @PostMapping("/mess")
+    public String PostAddMess(@ModelAttribute("messAdd") MessCancellations m, Model model, HttpSession session){
+        if (!isAuthenticated(session)) {
+            return "redirect:/";
+        }
+        System.out.println(messCancellationsService);
+        addDefaultAttributes(model,session);
+       Date utildate=null;
+       utildate=m.getDate();
+       java.sql.Date sqlDate = new java.sql.Date(utildate.getTime());
+        m.setDate(sqlDate);
+         messCancellationsService.createMessCancellation(m);
+        return "redirect:/mess";
     }
 }
