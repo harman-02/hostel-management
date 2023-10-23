2bf779604538029403f283b4d5c58cac7b8bbaa9
harman-02 <harmanjot.singh.cse21@itbhu.ac.in>
2023-10-23 23:47:53 +0530

    added filters

--- src/main/java/com/hms/HostelManagement/repository/MessCancellationsRepository.java
+++ src/main/java/com/hms/HostelManagement/repository/MessCancellationsRepository.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import java.util.Date;
 import java.util.List;
